8730f9d4196b2f99231b997e81775a56155b63ab
lyj00912 <3122742760@qq.com>
2022-02-06 17:17:56 +0800

    created new project UnitySocket, added some basic FPS Showing stuffs to detect the influence from socket in the future

---
+++ UnitySocket/Assets/FPS Scipts/MoveBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour {
+
+	Vector3 position;
+	float maxX = 3.0f;
+	float speed = 0.02f;
+	bool direction = true;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Step in
+		if (direction) {
+			position.x += speed;
+		} else {
+			position.x -= speed;
+		}
+
+		// Clamp
+		if (position.x >= maxX || position.x <= -maxX) {
+			direction = !direction;
+		}
+
+		// Apply
+		transform.position = position;
+	}
+}
---
+++ UnitySocket/Assets/FPS Scipts/ShowFPS.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowFPS : MonoBehaviour {
+
+	public float deltaTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+		float fps = 1.0f / deltaTime;
+		string fpsString = "FPS: " + fps.ToString ();
+
+		GUILayout.BeginArea (new Rect (0, 0, 130, 130));
+		GUILayout.TextField (fpsString);
+		GUILayout.EndArea ();
+	}
+}
